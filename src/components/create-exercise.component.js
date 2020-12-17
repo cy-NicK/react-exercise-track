b29146a8f4042bc009945d5172c2a3ee36343d7a
cy-NicK <48745785+cy-NicK@users.noreply.github.com>
2020-12-17 16:18:59 +0530

    Update create-exercise.component.js

--- src/components/create-exercise.component.js
+++ src/components/create-exercise.component.js
@@ -22,7 +22,7 @@ export default class CreateExercise extends Component{
     }
 
     componentDidMount(){
-        axios.get("http://localhost:5000/users/")
+        axios.get("https://back-exercise.herokuapp.com/users/")
             .then(response=>{
                 if(response.data.length>0){
                     this.setState({
@@ -71,7 +71,7 @@ export default class CreateExercise extends Component{
         };
 
         console.log(exercise);
-        axios.post("http://localhost:5000/exercises/add",exercise)
+        axios.post("https://back-exercise.herokuapp.com/exercises/add",exercise)
             .then(res=>console.log(res.data));
 
         window.location='/';
