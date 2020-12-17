5f648f913c4e6f387ab74544e092f0991e36102e
cy-NicK <48745785+cy-NicK@users.noreply.github.com>
2020-12-17 16:19:26 +0530

    Update create-user.component.js

--- src/components/create-user.component.js
+++ src/components/create-user.component.js
@@ -27,7 +27,7 @@ export default class CreateUser extends Component{
         };
 
         console.log(newUser);
-        axios.post('http://localhost:5000/users/add',newUser)
+        axios.post('https://back-exercise.herokuapp.com/users/add',newUser)
             .then(res=>console.log(res.data));
 
         this.setState({username: ''});
